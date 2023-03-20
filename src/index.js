a4117dc577c69089cba8a8654f31b75d26989a46
Peltoche <pierre.peltier@protonmail.com>
2023-03-20 16:12:04 +0100

    Upload correctly the files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-const {
-  BaseKonnector,
-  requestFactory,
-  scrape,
-  log,
-  utils
-} = require('cozy-konnector-libs')
+const { BaseKonnector, log } = require('cozy-konnector-libs')
 const axios = require('axios')
 
 module.exports = new BaseKonnector(start)
 
 const batchSize = 10
 
-async function start(fields, cozyParameters) {
+async function start(fields) {
   log('info', 'Authenticating ...')
 
+  let roomID
   // we know that if the first character is a '#', we have an alias not an id
   if (fields.roomID[0] === '#') {
     log('info', 'Fetch room id')
@@ -46,19 +41,27 @@ async function start(fields, cozyParameters) {
     }
   })
 
-  const msgs = res.data.chunk
+  let msgs = res.data.chunk
 
   if (msgs.length === 0) {
     log('info', 'There is no event in the room')
     return
   }
 
-  this.saveFiles(processMessages(fields.instanceURL, msgs))
-  log('info', 'foobar 2')
+  const encryptedMessages = msgs.find(e => e.type === 'm.room.encrypted')
+  if (encryptedMessages) {
+    throw new Error('encrypted rooms are not supported yet')
+  }
+
+  const files = processMessages(fields.instanceURL, msgs)
+  if (files.length > 0) {
+    log('info', `save ${files.length} files`)
+    this.saveFiles(files, fields)
+  }
 
   let lastEventId = encodeURIComponent(msgs[msgs.length - 1].event_id)
 
-  while (true) {
+  while (msgs.length === batchSize) {
     log('info', 'Fetch more messages')
     res = await axios({
       baseURL: fields.instanceURL,
@@ -68,12 +71,12 @@ async function start(fields, cozyParameters) {
       }
     })
 
-    const msgs = res.data.events_before
+    msgs = res.data.events_before
 
-    this.saveFiles(processMessages(fields.instanceURL, msgs))
-
-    if (msgs.length < batchSize) {
-      break
+    const files = processMessages(fields.instanceURL, msgs)
+    if (files.length > 0) {
+      log('info', `save ${files.length} files`)
+      this.saveFiles(files, fields)
     }
 
     lastEventId = encodeURIComponent(msgs[msgs.length - 1].event_id)
@@ -92,10 +95,11 @@ function processMessages(instanceURL, messages) {
         // We want to convert this mxc url into an http one looking like:
         // `https://{instanceURL}/_matrix/media/r0/download/{instanceName}/{some-id}`
         const parts = mxcUrl.split('/')
-        log('info', 'foobar')
         return {
+          fileIdAttributes: ['fileurl'],
           fileurl: `${instanceURL}/_matrix/media/r0/download/${parts[2]}/${parts[3]}`,
-          filename: 'foo.png'
+          filename: msg.content.body,
+          contentType: msg.content.info.mimetype
         }
       })
   )
